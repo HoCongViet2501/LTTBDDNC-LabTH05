5f80f79d8217566077870134f76ee58e28cdfcdd
HoCongViet2501 <viet.forwork.2501@gmail.com>
2022-09-30 11:34:45 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import data from './Data';
+
+const DATA = data;
+const Item = ({ item }) => (
+  <View style={styles.item}>
+    <Image source={item.img}></Image>
+    <View style={styles.centerItem}>
+      <Text>{item.title}</Text>
+      <Text>Shop:{item.shopName}</Text>
+    </View>
+    <TouchableOpacity style={{ backgroundColor: "red", borderRadius: 5 }}>
+      <Text>Chat</Text>
+    </TouchableOpacity>
+  </View>
+)
 
 export default function App() {
+  const renderItem = ({ item }) => {
+    return (
+      <Item item={item} />
+    )
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ alignItems: 'center', marginTop: 50 }}>
+        <Text style={{ fontWeight: "bold", fontSize: 16 }}>Ban co thac mac gi dung ngai chat voi shop</Text>
+      </View>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}>
+      </FlatList>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
-});
+  item: {
+    display: 'flex',
+    borderBottomColor: "gray",
+    borderTopColor: "gray",
+    backgroundColor: "green"
+  },
+  centerItem: {
+    flex: 1,
+    fontSize: 14
+  }
+})
+
+
---
+++ Data.js
@@ -0,0 +1,45 @@
+const data = [
+    {
+        id: 1,
+        title: "Ca nau lau,nau mi mini",
+        shopName: " Devang",
+        img: require("./assets/image/ca_nau_lau (1).png")
+    },
+    {
+        id: 2,
+        title: "1Kg ga bo toi",
+        shopName: " LBT Food",
+        img: require("./assets/image/ga_bo_toi.png")
+    },
+    {
+        id: 3,
+        title: "Xe can cau da nang",
+        shopName: "The gioi do choi",
+        img: require("./assets/image/xa_can_cau.png")
+    },
+    {
+        id: 4,
+        title: "Do choi dang mo hinh",
+        shopName: "The gioi do choi",
+        img: require("./assets/image/do_choi_dang_mo_hinh.png")
+    },
+    {
+        id: 5,
+        title: "Lanh dao gian don",
+        shopName: "Minh Long Book",
+        img: require("./assets/image/ca_nau_lau (1).png")
+    },
+    {
+        id: 6,
+        title: "Hieu long con tre",
+        shopName: "Minh Long Book",
+        img: require("./assets/image/hieu_long_con_tre.png")
+    },
+    {
+        id: 7,
+        title: "Donal trump thien tai lanh dao",
+        shopName: "Minh Long Book",
+        img: require("./assets/image/trump 1.png")
+    }
+]
+export default data;
